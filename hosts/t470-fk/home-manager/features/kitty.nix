5aba3ea1c6aecf69a5bc0e76e05921bf558ae5a6
Faye <fayeklass@outlook.com>
2024-04-08 23:38:15 +0200

    kitty config stuff

--- hosts/t470-fk/home-manager/features/kitty.nix
+++ hosts/t470-fk/home-manager/features/kitty.nix
@@ -5,9 +5,12 @@
 }: {
   programs.kitty = {
     enable = true;
+    font.name = "JetBrainsMono";
+    font.size = 12;
       settings = {
         foreground = "#${config.colorScheme.palette.base05}";
         background = "#${config.colorScheme.palette.base00}";
+        background_opacity = "0.9";
       };
     };
   }
