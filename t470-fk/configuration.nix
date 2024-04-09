7556704b79139c49612a0a4d71574a2c6e58d54a
Faye <fayeklass@outlook.com>
2024-04-09 03:33:03 +0200

    added hyprland modules

--- t470-fk/configuration.nix
+++ t470-fk/configuration.nix
@@ -92,9 +92,6 @@
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
     NIXOS_OZONE_WL = "1";
